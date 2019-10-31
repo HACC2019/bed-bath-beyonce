3e9bcc950d7732d0e4710cf54c1391d77006e9fb
Christopher-Sparks <csparks1@my.hpu.edu>
2019-10-30 18:05:34 -1000

    Added homepage route

--- application/app.js
+++ application/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressSession = require('express-session');
 
+//Routes
+const homepage = require('./routes/route-homepage');
+
 //Start express server
 const app = express();
 const port = 6066;
@@ -10,7 +13,11 @@ const port = 6066;
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(assetsPath));
 
+//Set routes pathways
+app.use('/')
 
+//Set server to listen to listen on defined port and log startup console
 app.listen(port);
 console.log(`Now listening on port ${port}`);
