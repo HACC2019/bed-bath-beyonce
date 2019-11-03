1d67096f52683e40f17a312f3ca779520a41e5f6
Christopher-Sparks <csparks1@my.hpu.edu>
2019-11-02 16:10:00 -1000

    Changed initial route to redirect to login page

--- application/routes/route-homepage.js
+++ application/routes/route-homepage.js
@@ -1,12 +1,9 @@
 const express = require('express');
 var router = express.Router();
 
-var User = require('../models/model-user');
-
 router.get('/', function(req, res){
 
-
-    res.render('view-homepage', {layout: false});
+    res.redirect('/login');
 });
 
 module.exports = router;
