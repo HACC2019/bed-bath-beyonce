1dcea3a4d1419820cc0aff6f7042e4b5aba48376
Christopher-Sparks <csparks1@my.hpu.edu>
2019-11-02 19:56:31 -1000

    Integrated all of PJ's pages into NodeJS server

--- application/routes/route-homepage.js
+++ application/routes/route-homepage.js
@@ -3,7 +3,42 @@ var router = express.Router();
 
 router.get('/', function(req, res){
 
-    res.redirect('/login');
+    res.redirect('/home-page');
+});
+
+router.get('/home-page', function(req, res){
+
+    res.render('view-home-page', {
+        title: 'home-page'
+    });
+});
+
+router.get('/contact', function(req, res){
+
+    res.render('view-contact', {
+        title: 'contact'
+    });
+});
+
+router.get('/landing-page', function(req, res){
+
+    res.render('view-landing-page', {
+        title: 'landing-page'
+    });
+});
+
+router.get('/project-page', function(req, res){
+
+    res.render('view-project-page', {
+        title: 'project-page'
+    });
+});
+
+router.get('/projects-with-sidebar', function(req, res){
+
+    res.render('view-projects-with-sidebar', {
+        title: 'projects-with-sidbar'
+    });
 });
 
 module.exports = router;
