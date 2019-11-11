f8da7c19ed5962b50d963d374a364f8acd91a21b
bryantsa <bryantsa@hawaii.edu>
2019-11-10 15:19:36 -1000

    added account change email

--- application/routes/route-account.js
+++ application/routes/route-account.js
@@ -13,10 +13,61 @@ router.get('/', function(req, res){
 
 router.post('/change', function(req, res){
 
-    User.findOne({_id: req.session.user._id}, function(err, result){
-        if(err) throw err;
+    User.findOne({_id: req.session.user._id})
+    .then(function(result){
+        if (req.body.fname != '') {
+            result.fname = req.body.fname;
+        }
+        if (req.body.lname != '') {
+            result.lname = req.body.lname;
+        }
+        if (req.body.organization!= '') {
+            result.organization = req.body.organization;
+        }
+        if (req.body.school != '') {
+            result.school = req.body.school;
+        }
+        if (req.body.password != '') {
+            result.password = req.body.password;
+        }
 
+        if (req.body.email != '') {
+            result.email = req.body.email;
+            req.check('email', 'Invalid email address').isEmail();
+            req.check('email', 'Email already exists').custom(function (value) {
+                return new Promise(function (resolve, reject) {
+                    User.findOne({ email: value }, function (err, result) {
+                        if (err) throw err;
+                        if (result == null) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                });
+            });
+        }
+        var errors = req.getValidationResult();
+
+        errors.then(function (errors) {
+            req.session.account_errors = errors.array({ onlyFirstError: true });
+            if (req.session.account_errors.length > 0) {
+                console.log('account changes contains errors');
+                res.redirect('/account');
+            }
+            else {
+                result.save()
+                    .then(function (result) {
+                        req.session.user = result;
+                        req.session.account_success = 'Account successfully changed'
+                        res.redirect('/account');
+                    });
+            }
+        });
     });
+
+    
+
 });
 
 module.exports = router;
